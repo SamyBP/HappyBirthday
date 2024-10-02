27ec49120e8f02df591ddaed1d8a129004d1ade3
Beni Pintea <b.pintea04@gmail.com>
2024-10-02 23:14:18 +0300

    added whatsapp message sending

--- README.md
+++ README.md
@@ -3,10 +3,14 @@
 ## Description
 
 HappyBirthday is a personal project to help automate sending Happy Birthday messages to your phone contacts
-It uses the Google Contacts API to fetch your contacts and WhatsApp API to send a personalized Happy Birthday message when one of your contacts celebrate theirs birthday
+It uses the Google Contacts API to fetch your contacts and PyWhatKit to send a personalized Happy Birthday message when one of your contacts celebrate theirs birthday
+The script will first fetch your contacts, retrieving the contact names, birthdays and phoneNumbers
+By default the script will only check for the first birthday and phone number saved for any of your contacts and send the Happy Birthday message 
+if the contact celebrates his birthday.
 
 ## Prerequisites 
 
+* Have python 3.11.* installed
 * Go to **Google Cloud Console** and create a new project
 * Enable **Google People API** from **API & Services > Library**
 * Create credentials: Choose OAuth Client ID and for client type choose Desktop Application
@@ -25,3 +29,13 @@ It uses the Google Contacts API to fetch your contacts and WhatsApp API to send 
 * Install the libraries:
 
       pip install -r requirements.txt
+
+* Creat a .env and enter your Happy Birthday message:
+
+      MESSAGE_TO_SEND="La Multi Ani!!"
+
+## Usage
+
+* Run the script:
+
+      python main.py
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
 import os.path
+from datetime import datetime
 
+import pywhatkit
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -35,12 +38,18 @@ def get_contacts(service):
         .connections()
         .list(
             resourceName="people/me",
-            pageSize=10,
-            personFields="names,emailAddresses",
+            pageSize=60,
+            personFields="names,birthdays,phoneNumbers",
         )
         .execute()
     )
     return results.get("connections", [])
+
+
+def celebrates_birthday(date, current_date):
+    if date['month'] != current_date.month or date['day'] != current_date.day:
+        return False
+    return True
 
 
 if __name__ == "__main__":
@@ -48,11 +57,20 @@ if __name__ == "__main__":
         credentials = get_credentials("token.json", "client-secret.json")
         people_service = get_people_service(credentials)
         contacts = get_contacts(people_service)
+        cdt = datetime.now()
+        msg = os.getenv("MESSAGE_TO_SEND")
+        with open("data.json", "w") as data_file:
+            json.dump(contacts, data_file)
         for contact in contacts:
             names = contact.get("names", [])
-            if names:
+            birthdays = contact.get("birthdays", [])
+            phone_numbers = contact.get("phoneNumbers", [])
+            if names and birthdays and phone_numbers:
                 name = names[0].get("displayName")
-                print(name)
+                birthday = birthdays[0].get("date")
+                phone_number = phone_numbers[0].get("canonicalForm")
+                print(f"{name} with birthday on: {birthday} and phone number:{phone_number}")
+                if celebrates_birthday(birthday, cdt):
+                    pywhatkit.sendwhatmsg_instantly(phone_no=phone_number, message=msg)
     except HttpError as error:
         print(error)
-
